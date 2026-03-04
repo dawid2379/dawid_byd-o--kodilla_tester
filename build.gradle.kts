e485a84efbfc00cce420ca9dafac3629bbdecd92
Dawid Bydłoń <dawid2379@gmail.com>
2026-03-04 11:55:57 +0100

    Initial version

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+git config --global user.name "Jan Kowalski"plugins {
+    id("java")
+}
+
+group = "com.kodilla"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
