9456eca7829bbba46c2e94b3309ede6cfb822e1d
Dawid Bydłoń <dawid2379@gmail.com>
2026-03-04 17:58:16 +0100

    mainextended

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,18 @@ public class Main
 {
     public static void main(String[] args) {
         String myName = "Maciej";
+        String mySurname = "Kowalski";
         int myAge = 22;
         double height = 1.78;
         char carClass = 'A';
         boolean isGoodProgrammer = true;
+
+        System.out.println(myName);
+        System.out.println(mySurname);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+
     }
 }
