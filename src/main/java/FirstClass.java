eb10d02b0facd1ef92111e83b39d5ecd63957f4e
Dawid Bydłoń <dawid2379@gmail.com>
2026-03-04 15:03:20 +0100

    zadanie_zmienne

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,7 @@
 public class FirstClass{
-    public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
+    public static void main(String[] args)
+    {
+        String zmienna = "Hello FirstClass!";
+                System.out.println(zmienna);
     }
 }
---
+++ src/main/java/zadanie_zmienne.java
@@ -0,0 +1,20 @@
+public class zadanie_zmienne
+{
+    public static void main(String[] args)
+    {
+        String imie = "Dawid";
+        int wiek = 21;
+        double wzrost = 1.80;
+        char pierwszaliteranazwiska = 'D';
+        boolean slonceswieci = true;
+        long numertelefonu = 123456789;
+
+        System.out.println(imie);
+        System.out.println(wiek);
+        System.out.println(wzrost);
+        System.out.println(pierwszaliteranazwiska);
+        System.out.println(slonceswieci);
+        System.out.println(numertelefonu);
+
+    }
+}
